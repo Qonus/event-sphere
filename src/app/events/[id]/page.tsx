c55688b61c8b31c81570243072c718a54c119a31
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-12-12 19:38:23 +0500

    [Add] event page

---
+++ src/app/events/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Event from "@/compnents/EventCompnent/Event"
+
+export default function EventPage(){
+  return(
+    <Event 
+          title="placeholder TITLE"
+          start_date="07.03.2024" 
+          end_date="08.04.2024"
+          image={"/icon.png"} 
+          description={"дыопдыофдвы"}    />
+  )
+}
