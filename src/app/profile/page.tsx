e959fa7523975caa93cfe573dcacf2998613a4d4
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-12-13 02:01:22 +0500

    [CHange] event card data looks

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import ProfileCard from "@/compnents/ProfileCardComponent/ProfileCard";
 import { redirect } from 'next/navigation'
 import styles from "./page.module.scss"
+import EventCard from "@/compnents/EventCardComponent/EventCard";
 
 export default async function Profile(){
     const session = await auth();
@@ -13,7 +14,16 @@ export default async function Profile(){
     return(
       <div className={styles.profile_page}>
         <div className="hr">Добро пожаловать, {session?.user?.name}</div>
-        <ProfileCard name={session?.user?.name} email={session?.user?.email} image={session?.user?.image}/>
+          <div className={styles.profile_page_wrapper}>
+            <ProfileCard name={session?.user?.name} email={session?.user?.email} image={session?.user?.image}/>
+
+            <div className={styles.profile_page__events}>
+                <EventCard></EventCard>
+                <EventCard></EventCard>
+                <EventCard></EventCard>
+                <EventCard></EventCard>
+            </div>
+          </div>
       </div>
     )
 }
--- src/compnents/EventCardComponent/EventCard.tsx
+++ src/compnents/EventCardComponent/EventCard.tsx
@@ -3,26 +3,41 @@ import styles from "./EventCard.module.scss";
 import Link from "next/link";
 
 export default function EventCard({
-  href,
+  href="/",
   title = "Placegolder TITLE",
   description = "placegoler DESC",
   likes = 0,
   tags = ["Тэг Плейсхолдер", "Убрать тэги позже","Тэг Плейсхолдер", "Убрать тэги позже"],
-  img = "/icon.png"
+  img = "/icon.png",
+  start_date = "00.00.0000",
+  end_date="00.00.0000",
 }: {
-  href: string;
+  href?: string;
   title?: string;
   description?: string;
   likes?: number;
   tags?: string[];
   img?: string;
+  start_date?:string;
+  end_date?:string;
 }) {
+
   return (
     <Link
       href={href}
       className={styles.event_card + " glass"}
       style={{ textDecoration: "none" }}
     >
+      <div className={styles.event_card__data}>
+          <div className={styles.event_card__data__status}>
+            Upcoming
+          </div>
+
+          <div className={styles.event_card__data__dates}>
+            {start_date}-{end_date}
+          </div>
+      </div>
+
       <div className={styles.event_card__image_wrapper}>
           <Image 
           className={styles.event_card__image_wrapper__image}
@@ -64,6 +79,3 @@ export default function EventCard({
     </Link>
   );
 }
-{/* 
-
-        */}
