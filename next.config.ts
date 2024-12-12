3f574fc95d025077fa20c72b2986cef8473a1645
Qonus <a1bat111k@mail.ru>
2024-12-12 11:53:56 +0500

    Sass setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      }
+    ]
+  },
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "styles")],
+    prependData: `@import "main.scss";`,
+  }
 };
 
 export default nextConfig;
