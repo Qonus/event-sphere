37f8fa2a447e4b804dcb186bd064c40148df9f82
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-12-12 15:19:44 +0500

    [Change] colors

--- src/compnents/HomepageComponent/Homepage.tsx
+++ src/compnents/HomepageComponent/Homepage.tsx
@@ -5,6 +5,7 @@ export default async function Homepage(){
     return(
         <div className={styles.homepage}>
             <div className={styles.homepage_wrapper}>
+                <h1 style={{color: '$foreground-color-1'}}> This is Event Sphere! </h1>
                 <div className={styles.homepage__map}>
                     <LeafletMap />
                 </div>
