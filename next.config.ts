c2265eb2e8d3132daf57f8863e93b0908100b1d3
Qonus <94901082+Qonus@users.noreply.github.com>
2025-03-16 23:29:53 +0500

    [Fix] NEXTAUTH_URL

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
+    NEXTAUTH_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
   },
   typescript: {
     ignoreBuildErrors: true,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Homepage from "../compnents/HomepageComponent/Homepage";
 
 export default function Home(){
+  console.log(process.env.NEXTAUTH_URL);
   return(
     <Homepage />
   )
