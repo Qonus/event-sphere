84541b9f852acaa6f9fc691e6136d55832f2a829
Qonus <94901082+Qonus@users.noreply.github.com>
2025-03-16 23:25:26 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_URL: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
